444d006eb39e49ebe626eb8e2520535a405dab39
A.P.Jovin <apjovin@leafsecurity.ai>
2026-07-13 15:01:54 +0000

    bump: upgrade version to v2.0.4

--- leafscan/__init__.py
+++ leafscan/__init__.py
@@ -8,7 +8,7 @@ GitHub:  https://github.com/Jovinap/leafscan
 License: MIT
 """
 
-__version__     = "2.0.0"
+__version__     = "2.0.4"
 __author__      = "A.P.Jovin"
 __company__     = "JJ Groups of Company"
 __product__     = "Leaf Security AI"
